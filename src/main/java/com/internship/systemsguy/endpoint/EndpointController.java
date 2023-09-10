bf79a813c2338dd4193ccc42f2b52c46735b8662
Gifted-Labs <juliusadjeteysowah@gmail.com>
2023-09-10 15:51:43 +0000

    Now everything is working so left for me to host it and try it out

--- src/main/java/com/internship/systemsguy/endpoint/EndpointController.java
+++ src/main/java/com/internship/systemsguy/endpoint/EndpointController.java
@@ -4,6 +4,7 @@ import com.internship.systemsguy.student.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EndpointController {
     
     @GetMapping("/name")
-    public Student getDetails(){
-        Student student = new Student("Julius","backend");
+
+    public Student myEndpoint(@RequestParam(value="slack_name") String slack_name, @RequestParam(value="track") String track){
+        Student student = new Student(slack_name,track);
         return student;
     }
 }
