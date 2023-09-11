f9719a0a8c18ea819e330f699b3a85fc3e2ea5bb
Gifted-Labs <juliusadjeteysowah@gmail.com>
2023-09-11 05:11:07 +0000

    Comments were added

--- src/main/java/com/internship/systemsguy/endpoint/EndpointController.java
+++ src/main/java/com/internship/systemsguy/endpoint/EndpointController.java
@@ -1,10 +1,19 @@
 package com.internship.systemsguy.endpoint;
 
+/**
+ * @author Julius
+ * This is a Java class named  EndpointController  which is annotated with  @RestController  to indicate *that it is a RESTful controller. It handles HTTP requests and returns JSON responses. 
+ */
+
 import com.internship.systemsguy.student.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+The class has a single endpoint  /api  which is mapped to the  myEndpoint  method using the  @GetMapping  annotation. The  myEndpoint  method accepts two query parameters  slack_name  and  track  using the  @RequestParam  annotation. 
+*/
 
 @RestController
 @RequestMapping("/api")
